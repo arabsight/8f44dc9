287cb267c4c71d8d94efc95be75967c00fc4d7f4
Rabah Ghodbane <r-ghodbane@live.com>
2016-03-29 21:01:02 +0100

    base class entity vm

---
+++ Expenses.Desktop/Common/EntityViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using Expenses.Core.Shared;
+using Expenses.Logic;
+
+namespace Expenses.UI.Common
+{
+    [POCOViewModel]
+    public class EntityViewModel<TEntity> : IDocumentContent where TEntity : class
+    {
+        protected Service<TEntity> Service;
+
+        public EntityViewModel(Service<TEntity> service)
+        {
+            Service = service;
+        }
+
+        public virtual Session Session => Session.Default;
+
+        public virtual TEntity Entity { get; set; }
+
+        protected virtual IMessageBoxService MessageBoxService => null;
+
+        [Command(false)]
+        public virtual void OnClose(CancelEventArgs e)
+        {
+            if (!Service.IsDirty(Entity)) return;
+            var result = ShowConfirmation();
+
+            switch (result)
+            {
+                case MessageResult.Yes:
+                    if (!TrySave()) e.Cancel = true;
+                    break;
+                case MessageResult.Cancel:
+                    e.Cancel = true;
+                    break;
+                case MessageResult.None:
+                    break;
+                case MessageResult.OK:
+                    break;
+                case MessageResult.No:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        [Command(false)]
+        public virtual void OnDestroy()
+        {
+        }
+
+        public virtual IDocumentOwner DocumentOwner { get; set; }
+        public virtual object Title { get; }
+
+        protected virtual MessageResult ShowConfirmation()
+        {
+            return MessageBoxService.Show(
+                "Voulez vous enregistrer les modifications",
+                "Enregistrer",
+                MessageButton.YesNoCancel,
+                MessageIcon.Question,
+                MessageResult.No);
+        }
+
+        protected virtual bool TrySave()
+        {
+            if (!Service.IsDirty(Entity)) return true;
+
+            try
+            {
+                if (Service.State(Entity) == EntityState.Added)
+                {
+                    Service.Save(Entity);
+                    Messenger.Default.Send(new EntityMessage<TEntity>(Entity, MessageType.Added));
+                }
+                else
+                {
+                    ((ITrackable) Entity).UpdatedBy = Session.Identity.Id;
+                    Service.Save(Entity);
+                    Messenger.Default.Send(new EntityMessage<TEntity>(Entity, MessageType.Modified));
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBoxService.Show(e.Message, "Validation");
+                return false;
+            }
+        }
+
+        public virtual void SaveAndClose()
+        {
+            if (TrySave()) DocumentOwner.Close(this, false);
+        }
+    }
+}
---
+++ Expenses.Logic/Helpers/ExpenseTotal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Expenses.Logic.Helpers
+{
+    public class ExpenseTotal
+    {
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
